0c1ff02629cf5304153b74944a0bd3e8190f6e54
QuantMis <realhost01@gmail.com>
2025-04-19 10:06:28 +0800

    Add dotenv reader to Program.cs to support .env based credentials

--- src/MerchantService/MerchatService.API/Program.cs
+++ src/MerchantService/MerchatService.API/Program.cs
@@ -1,10 +1,26 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using MerchatService.API.Infrastructure.Database;
+using dotenv.net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// read from dotenv
+DotEnv.Load(options: new DotEnvOptions(ignoreExceptions: false));
+var envVars = DotEnv.Read();
+var DB_NAME = envVars["DB_NAME"];
+var DB_USER = envVars["DB_USER"];
+var DB_PASS = envVars["DB_PASS"];
+var DB_PORT = envVars["DB_PORT"];
+
+string connectionString = $"Host=localhost;Port={DB_PORT};Database={DB_NAME};Username={DB_USER};Password={DB_PASS}";
+
+builder.Services.AddDbContext<MerchantDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
